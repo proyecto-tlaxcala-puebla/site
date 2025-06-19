92dd2daf7372b20843a18d2eaa06e6596d9a9156
jairomelo <jairomelo@users.noreply.github.com>
2025-06-19 18:51:38 +0000

    deploy: cd370a99bc9c3adac8f95c68f15a1c9a0124bd85

---
+++ _app/immutable/nodes/2.BhdOByjG.js
@@ -0,0 +1,1 @@
+import{f as o,a}from"../chunks/BU__R0oI.js";import"../chunks/CCzTL2eJ.js";import{n as r}from"../chunks/Bfmzv9Yw.js";var m=o('<h1>Welcome to SvelteKit</h1> <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>',1);function s(t){var e=m();r(2),a(t,e)}export{s as component};
