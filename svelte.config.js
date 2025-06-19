54e002176f207a4f9f4585cee2919ad8ffc846bd
jairomelo <jairoantoniomelo@gmail.com>
2025-06-19 11:45:32 -0700

    feat: add Inlang M Function Matcher plugin and project ID
    - Introduced a new plugin for the Inlang SDK to handle message function matching.
    - Added project ID for the Inlang project.
    - Enabled prerendering in the layout for improved performance.
    - Updated base path in svelte.config.js for production environment.

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		}),
 
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/memoriadelagua' : '' /* base path TBD */
+			base: process.env.NODE_ENV === 'production' ? '/site' : ''
 		}
 	},
 	extensions: ['.svelte', '.svx']
