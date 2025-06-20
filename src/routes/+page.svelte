d6e681cc7f62f01c0cd941ad68e3879d5478066f
jairomelo <jairoantoniomelo@gmail.com>
2025-06-20 10:07:32 -0700

    feat: update landing page with new content and background iframe

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div style="overflow:hidden; position: relative; width: 100%; height: 100vh;">
+  <iframe
+    src="https://glittery-clafoutis-927ba9.netlify.app/"
+    style="width: 100%; height: 100vh; position: absolute; top: 0; left: 0; z-index: 0; opacity: 0.15;"
+    frameborder="0"
+  ></iframe>
+
+  <div style="position: relative; z-index: 1; color: white; text-align: center; padding-top: 30vh;">
+    <h1>El río recuerda.<br />¿Y nosotros?</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </p>
+  </div>
+</div>
