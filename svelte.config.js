90f9a37e9b0610284650f5e93436eae98ae45fe1
jairomelo <jairoantoniomelo@gmail.com>
2025-10-05 13:25:59 -0700

    Remove unused import of 'prerendered' from svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { prerendered } from '$service-worker';
 
 const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
