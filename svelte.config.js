5731b6924e621bafa7272556286b690ce30181fa
jairomelo <jairoantoniomelo@gmail.com>
2025-10-05 13:22:03 -0700

    feat: add prerendering configuration to handle missing Ids when building the site

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { prerendered } from '$service-worker';
 
 const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { 
+	kit: {
+		prerender: {
+			handleMissingId: 'warn',
+		}, 
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
